82108cf4f88520db4eb4891dd05882ee588c772b
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-06-12 22:05:14 -0300

    feat: adicionar componentes de navegação e login, atualizar estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Aurora from './Aurora';
-  
+
+import Navigation from '@/Componentes/Navigation/Navigation';
 
 
 export default function Home() {
   return (
     <div>
-      <Aurora colorStops={["#086b05", "#196e08", "#155302"]} blend={1.0} amplitude={1.5} speed={1.5}/>
-      
+      <Aurora colorStops={["#0fb809", "#21c201", "#28a103"]} blend={10.0} amplitude={2.0} speed={2.0}/>
+        <div className='Main-panel'>
+          <Navigation />
+        </div>
     </div>
   );
 }
+
